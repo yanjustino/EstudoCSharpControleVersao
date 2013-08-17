a86d8cf3a4f76c192f7ed4718106175183037a09
Yan Justino <yanlimaj@gmail.com>
2013-08-16 23:36:14 -0300

    Estudo C#
    versão C#

---
+++ EstudoCSharp/ControladorSistema.cs
@@ -0,0 +1,54 @@
+﻿
+using System;
+namespace EstudoCSharp
+{
+    /*
+     * Classe Sistema implementada no arquivo Sistema.java
+     */
+    class ControladorSistema
+    {
+        static void Main(string[] args)
+        {
+            Sistema sistema = new Sistema();
+            sistema.Versao = 1.0;
+
+            /*
+             * Método pra mostrar qual é a versão do sistema Implementado logo
+             * abaixo do método Main
+             */
+            MensagemDaVersaoDoSistema(sistema);
+
+            /*
+             * Fazendo upgrade de versão com while
+             */
+            while (sistema.Versao < 10)
+            {
+                double versao = sistema.Versao;
+                Console.WriteLine(versao);
+                sistema.Versao += 1;
+            }
+
+            /*
+             * Método pra mostrar qual é a versão do sistema Implementado logo
+             * abaixo do método Main
+             */
+            MensagemDaVersaoDoSistema(sistema);
+        }
+
+        /*
+         * Método pra mostrar qual é a versão do sistema Recebe por parâmetro um
+         * Sistema
+         */
+        public static void MensagemDaVersaoDoSistema(Sistema sistema)
+        {
+            if (sistema.isVersaoDesenvolvimento())
+                Console.WriteLine("ATENÇÃO : Versão do sitema é de desenvolvimento");
+
+            else if (sistema.isVersaoBeta())
+                Console.WriteLine("ATENÇÃO : Versão do sitema é beta");
+
+            else if (sistema.isVersaoFinal())
+                Console.WriteLine("ATENÇÃO : Versão do sitema é a Final");
+        }
+    }
+}
---
+++ EstudoCSharp/Sistema.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EstudoCSharp
+{
+    public class Sistema
+    {
+        public double Versao { get; set; }
+        
+        /*
+         * Verifica se a versão é desenvolvimento.
+         * A versão é desenvolvimento se seu valor for menor que 1.0
+         */
+        public bool isVersaoDesenvolvimento()
+        {
+            return this.Versao < 1.0;
+        }
+
+        /*
+         * Verifica se a versão é beta.
+         * A versão é beta se seu valor for menor ou igual a 2.0
+         */
+        public bool isVersaoBeta()
+        {
+            return this.Versao <= 2.0;
+        }
+
+        /*
+         * Verifica se a versão Final.
+         * A versão é final se seu valor for maior que 2.0
+         */
+        public bool isVersaoFinal()
+        {
+            return this.Versao > 2.0;
+        }
+    }
+}
